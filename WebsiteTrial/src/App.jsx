a8b3d26cfcb37afce4bfc34f06d3e8a0473534be
Osman Gündüz <4242osman424@gmail.com>
2025-02-23 23:12:35 +0300

    Add routing and components for homepage, about, projects, and contact pages; include background image and update package dependencies

--- WebsiteTrial/src/App.jsx
+++ WebsiteTrial/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import background from "./assets/Background.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='w-screen h-screen bg-stone-800 text-white'>
-      Osman Gündüz
-    </div>
-  )
+    <Router>
+      <div
+        className="w-screen bg-no-repeat bg-cover bg-center"
+        style={{ backgroundImage: `url(${background})` }} // Doğru kullanım
+      >
+        <div className="w-full h-16 flex items-center shadow-sm px-44 select-none">
+          <Header />
+        </div>
+        <main className="w-full h-full flex flex-grow flex-col px-44 ">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ WebsiteTrial/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const headerTabs = [
+  {
+    title: "Home",
+    link: "/home",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Projects",
+    link: "/projects",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
+const Header = () => {
+    const navigate = useNavigate();
+  return (
+    <div className="w-full flex flex-row justify-between items-center">
+      <div className="cursor-pointer" onClick={()=> navigate("/home")}>
+        <h1 className="text-blue-500 font-bold text-[28px]">
+          Osman
+          <span className="text-blue-400 font-semibold">Gndz</span>
+        </h1>
+      </div>
+      <div className="flex flex-row gap-20 font-semibold text-[16px]">
+        {headerTabs.map((tab, index) => (
+          <div key={index} className="cursor-pointer" onClick={()=> navigate(tab.link) } >
+            {tab.title}
+          </div>
+        ))}
+        <div className="cursor-pointer">En</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
